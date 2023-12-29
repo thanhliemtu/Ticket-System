2f93a30732efd246ef1093e5058d84d8aff255c1
Thanh Liem Tu <97198800+thanhliemtu@users.noreply.github.com>
2023-12-29 08:27:38 +0000

    added two hello functions, with query and path extractors

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,13 @@
 
 use std::net::SocketAddr;
 
-use axum::{Router, response::Html, routing::get};
+use axum::{Router, response::{Html, IntoResponse}, routing::get, extract::{Query, Path}};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") })
-    );
-
+    let routes_hello = Router::new().merge(routes_hello());
+        
     // region:      --- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
@@ -21,4 +19,32 @@ async fn main() {
     // end region:  --- Start Server
 }
 
+// region:      --- Routes Hello
+fn routes_hello() -> Router {
+    Router::new()
+    .route("/hello", get(handler_hello))
+    .route("/hello2/:name", get(handler_hello2))
+}
 
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+// e.g., `/hello?name=Haruka`
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+    
+    let name = params.name.as_deref().unwrap_or("World!");
+
+    Html(format!("Hello <strong>{name}!/strong>"))
+}
+
+// e.g., `/hello2/Hansamu`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}!/strong>"))
+}
+
+// end region:  --- Handler Hello
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -7,7 +7,9 @@ async fn quick_dev() -> Result<()> {
 
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=Haruka").await?.print().await?;
+
+    hc.do_get("/hello2/Hansamu").await?.print().await?;
 
     Ok(())
 }
