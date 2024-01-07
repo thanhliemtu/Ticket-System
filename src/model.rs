d18ec030b3a66b586f28ff73c50bf1704d8f9ca7
Thanh Liem Tu <97198800+thanhliemtu@users.noreply.github.com>
2024-01-07 00:04:28 +0000

    added Ctx for REST handlers

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,7 @@
 //! Simplistic Model Layer
 //! (with mock-store layer)
 
-use crate::{Error, Result};
+use crate::{Error, Result, ctx::Ctx};
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 
@@ -9,6 +9,7 @@ use std::sync::{Arc, Mutex};
 #[derive(Clone, Debug, Serialize)]
 pub struct Ticket {
     pub id: u64,
+    pub cid: u64, // creator user_id
     pub title: String,
 }
 
@@ -35,12 +36,17 @@ impl ModelController {
 
 // CRUD implementation
 impl ModelController {
-    pub async fn create_ticket(&self, ticket_fc: TicketForCreate) -> Result<Ticket> {
+    pub async fn create_ticket(
+        &self, 
+        ctx: Ctx,
+        ticket_fc: TicketForCreate
+    ) -> Result<Ticket> {
         let mut store = self.tickets_store.lock().unwrap();
 
         let id = store.len() as u64;
         let ticket = Ticket {
             id,
+            cid: ctx.user_id(),
             title: ticket_fc.title
         };
         store.push(Some(ticket.clone()));
@@ -48,7 +54,7 @@ impl ModelController {
         Ok(ticket)
     }
 
-    pub async fn list_tickets(&self) -> Result<Vec<Ticket>> {
+    pub async fn list_tickets(&self, _ctx: Ctx) -> Result<Vec<Ticket>> {
         let store = self.tickets_store.lock().unwrap();
 
         // filter_map excludes None variant
@@ -58,7 +64,7 @@ impl ModelController {
         Ok(tickets)
     }
 
-    pub async fn delete_ticket(&self, id: u64) -> Result<Ticket> {
+    pub async fn delete_ticket(&self, _ctx: Ctx, id: u64) -> Result<Ticket> {
         let mut store = self.tickets_store.lock().unwrap();
 
         let ticket = store.get_mut(id as usize).and_then(|t| t.take());
